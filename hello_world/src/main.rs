fcb7ef90cdf9587dcbbad9e0d2a0c1f4ae3321ac
gnkm <genki.matsunaga@gmail.com>
2025-05-31 16:08:30 +0900

    数値系変数定義

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,3 +1,42 @@
+use indexmap::IndexMap;
+use std::collections::HashMap;
+
 fn main() {
-    println!("Hello, world!");
+    let num1: i8 = 1;
+    let num2: f32 = 2.0;
+    let num3 = 3.0;
+
+    println!("num1: {}", num1);
+    println!("num2: {}", num2);
+    println!("num3: {}", num3);
+
+    let mut map1: HashMap<String, i8> = HashMap::new();
+    map1.insert(String::from("num1"), 1);
+    map1.insert(String::from("num2"), 2);
+    map1.insert(String::from("num3"), 3);
+    map1.insert(String::from("num4"), 4);
+    map1.insert(String::from("num5"), 5);
+    map1.insert(String::from("num6"), 6);
+    map1.insert(String::from("num7"), 7);
+    map1.insert(String::from("num8"), 8);
+    map1.insert(String::from("num9"), 9);
+
+    for (key, value) in &map1 {
+        println!("key: {}, value: {}", key, value);
+    }
+
+    let mut map2: IndexMap<String, i8> = IndexMap::new();
+    map2.insert(String::from("num1"), 1);
+    map2.insert(String::from("num2"), 2);
+    map2.insert(String::from("num3"), 3);
+    map2.insert(String::from("num4"), 4);
+    map2.insert(String::from("num5"), 5);
+    map2.insert(String::from("num6"), 6);
+    map2.insert(String::from("num7"), 7);
+    map2.insert(String::from("num8"), 8);
+    map2.insert(String::from("num9"), 9);
+
+    for (key, value) in &map2 {
+        println!("key: {}, value: {}", key, value);
+    }
 }
